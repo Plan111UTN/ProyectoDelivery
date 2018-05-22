35d14bfdc7d05939744535b2c960fa4996c2d06f
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2018-05-21 22:59:34 -0300

    creación de la clase Vehiculo

---
+++ src/main/java/com/plan111/modelo/Vehiculo.java
@@ -0,0 +1,12 @@
+package com.plan111.modelo;
+
+/**
+ * @author Guido
+ */
+
+public class Vehiculo {
+  private String patente;
+  private String modelo;
+  private String marca;
+  private TipoVehiculo tipoVehiculo;
+}
