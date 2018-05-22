53c02ec4f136226049c79ba0521b66fcd7351825
Guido Cavallo <SrShark@users.noreply.github.com>
2018-05-22 20:30:59 -0300

    agregado anotaciones de hibernate a las clase de PersonalEntrega

--- src/main/java/com/plan111/modelo/EstadoPersonalEntrega.java
+++ src/main/java/com/plan111/modelo/EstadoPersonalEntrega.java
@@ -1,6 +1,23 @@
 package com.plan111.modelo;
 
-public class EstadoPersonalEntrega {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "estado_personal_entrega")
+@Getter @Setter @NoArgsConstructor
+public class EstadoPersonalEntrega implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer idEstadoPersonalEntrega;
+
+  @Column(name = "nombre")
   private  String nombre;
+
+  @Column(name = "descripcion")
   private  String descripcion;
 }
--- src/main/java/com/plan111/modelo/Vehiculo.java
+++ src/main/java/com/plan111/modelo/Vehiculo.java
@@ -1,13 +1,33 @@
 package com.plan111.modelo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
 /**
  * @author Guido
  */
 
+@Entity
+@Table(name = "vehiculo")
+@Setter @Getter @NoArgsConstructor
 public class Vehiculo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer idVehiculo;
+
+  @Column
   private String patente;
+
+  @Column
   private String modelo;
+
+  @Column
   private String marca;
+
+  @Column
   private TipoVehiculo tipoVehiculo;
 
   public void conocerTipoVehiculo() {
