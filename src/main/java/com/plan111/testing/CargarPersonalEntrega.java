f847e5d7c8c04782c3fdb9135d969e7626aceda7
Guido Cavallo <SrShark@users.noreply.github.com>
2018-05-29 17:33:28 -0300

    agregado paquete para testeo de clases

---
+++ src/main/java/com/plan111/testing/CargarPersonalEntrega.java
@@ -0,0 +1,19 @@
+package com.plan111.testing;
+
+import com.plan111.dao.PersonalEntregaDAO;
+import com.plan111.modelo.PersonalEntrega;
+import com.plan111.modelo.Vehiculo;
+
+/**
+ * @author Guido
+ */
+
+public class CargarPersonalEntrega {
+  public static void main(String[] args) {
+    PersonalEntrega cadete1 = new PersonalEntrega();
+    cadete1.setNumeroCarnet(123);
+
+    PersonalEntregaDAO nuevoCadete = new PersonalEntregaDAO();
+    nuevoCadete.registrarPersonalEntrega(cadete1);
+  }
+}
