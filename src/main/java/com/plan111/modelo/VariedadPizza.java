6d331bffc00e6ced1d35661bd03eeff63ce71fcc
Daniel Rosso <38544140+daniel1rosso@users.noreply.github.com>
2018-06-12 20:19:05 -0300

    borrado de clases repetidas en el dominio

--- src/main/java/com/plan111/modelo/VariedadPizza.java
+++ src/main/java/com/plan111/modelo/VariedadPizza.java
@@ -5,61 +5,28 @@
  */
 package com.plan111.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 /**
  *
- * @author Candelaria
+ * @author USUARIO
  */
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name="variedad_pizza")
 public class VariedadPizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     
-    private Integer id;
-    private String ingredientes;
+    @Column
     private String nombre;
-
-    /**
-     * Constructor por defecto de la clase VariedadPizza.
-     */
-    public VariedadPizza() {
-    }
-    
-    /**
-     * Constructor con todos los atributos de la clase VariedadPizza.
-     * @param ingredientes
-     * @param nombre 
-     */
-    public VariedadPizza(String ingredientes, String nombre) {
-        this.ingredientes = ingredientes;
-        this.nombre = nombre;
-    }
-
-    // A continuación se listan todos los métodos de seteo
-    // de cada atributo de la clase
-    
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    
-    public String getIngredientes() {
-        return ingredientes;
-    }
-
-    public void setIngredientes(String ingredientes) {
-        this.ingredientes = ingredientes;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    @Override
-    public String toString() {
-        return this.nombre;
-    }
+    @Column
+    private String descripcion;
 }
